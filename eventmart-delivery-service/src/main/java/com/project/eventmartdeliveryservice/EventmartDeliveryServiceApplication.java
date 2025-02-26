e0d958b15a9a3ffa4cbc4bb171f57bf1b02c27b5
Jimyeong Kim <kjm021221@icloud.com>
2025-02-26 17:47:39 +0900

    ✨feat: eventmart-delivery-service 모듈 생성

---
+++ eventmart-delivery-service/src/main/java/com/project/eventmartdeliveryservice/EventmartDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventmartdeliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmartDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventmartDeliveryServiceApplication.class, args);
+    }
+
+}
---
+++ eventmart-delivery-service/src/test/java/com/project/eventmartdeliveryservice/EventmartDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventmartdeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmartDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
