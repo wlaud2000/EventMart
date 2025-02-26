7e3636b48038a613952c85c9b93a5a993f256482
Jimyeong Kim <kjm021221@icloud.com>
2025-02-26 17:39:44 +0900

    ✨feat: eventmart-payment-service 모듈 생성

---
+++ eventmart-payment-service/src/main/java/com/project/eventmartpaymentservice/EventmartPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventmartpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmartPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventmartPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ eventmart-payment-service/src/test/java/com/project/eventmartpaymentservice/EventmartPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventmartpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmartPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
