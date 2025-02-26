95649c3e3278a5f93b22fbb36f92fe19043203b5
Jimyeong Kim <kjm021221@icloud.com>
2025-02-26 17:33:14 +0900

    ✨feat: eventmart-inventory-service 모듈 생성

---
+++ eventmart-inventory-service/src/main/java/com/project/eventmartinventoryservice/EventmartInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventmartinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmartInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventmartInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ eventmart-inventory-service/src/test/java/com/project/eventmartinventoryservice/EventmartInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventmartinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmartInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
