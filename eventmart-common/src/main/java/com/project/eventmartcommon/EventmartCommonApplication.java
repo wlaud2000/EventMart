fe00370ba0c0f8ef2c8244f003f709d8dc763b45
Jimyeong Kim <kjm021221@icloud.com>
2025-02-26 16:58:24 +0900

    ✨feat: eventmart-common 모듈 생성

---
+++ eventmart-common/src/main/java/com/project/eventmartcommon/EventmartCommonApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventmartcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmartCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventmartCommonApplication.class, args);
+	}
+
+}
---
+++ eventmart-common/src/test/java/com/project/eventmartcommon/EventmartCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventmartcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmartCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
