57916cfce20ddd4bf5b14aebc26950ffdd721522
Jimyeong Kim <kjm021221@icloud.com>
2025-02-26 17:29:09 +0900

    ✨feat: eventmart-order-service 모듈 생성 및 gradle 최적화

---
+++ eventmart-order-service/src/main/java/com/project/eventmartorderservice/EventmartOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventmartorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmartOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventmartOrderServiceApplication.class, args);
+	}
+
+}
---
+++ eventmart-order-service/src/test/java/com/project/eventmartorderservice/EventmartOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventmartorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmartOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
