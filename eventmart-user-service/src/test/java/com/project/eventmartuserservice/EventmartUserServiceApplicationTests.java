5560e4bd3ed9568c8b01dd76057906fa863c9d14
Jimyeong Kim <kjm021221@icloud.com>
2025-02-28 09:03:38 +0900

    ✨feat: eventmart-user-service 모듈 생성

---
+++ eventmart-user-service/src/test/java/com/project/eventmartuserservice/EventmartUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventmartuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmartUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
