983d5be80886e852537f044bf5a11975a7fa8310
Jimyeong Kim <kjm021221@icloud.com>
2025-03-01 14:48:09 +0900

    ✨feat: BaseEntity 생성 및 설정

--- eventmart-common/src/main/java/com/project/eventmartcommon/EventmartCommonApplication.java
+++ eventmart-common/src/main/java/com/project/eventmartcommon/EventmartCommonApplication.java
@@ -2,8 +2,10 @@ package com.project.eventmartcommon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //JPA Auditing 기능 활성화
 public class EventmartCommonApplication {
 
 	public static void main(String[] args) {
--- eventmart-order-service/src/main/java/com/project/eventmartorderservice/EventmartOrderServiceApplication.java
+++ eventmart-order-service/src/main/java/com/project/eventmartorderservice/EventmartOrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.project.eventmartorderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //JPA Auditing 기능 활성화
 public class EventmartOrderServiceApplication {
 
 	public static void main(String[] args) {
